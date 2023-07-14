ea28f043fdc052d73021b7f6612ed4fe2e05c6c1
Fran Blanco <arbust908@gmail.com>
2023-07-14 16:34:58 -0300

    feat: GH pages action 7

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/',
+  base: '/tomos-y-tumbas/',
   title: 'Tomos y Tumbas',
   description: 'The ruleset of Tomos y Tumbas TTRPG',
   themeConfig: {
